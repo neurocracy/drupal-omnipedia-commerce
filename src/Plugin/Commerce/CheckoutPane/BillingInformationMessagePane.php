889f5782709350fdfda7fc70ff2cd469c24160b6
Matei Stanca <i@ambientimpact.com>
2021-06-12 17:47:47 -0400

    Added Commerce checkout pane to explain why we need billing address.

---
+++ src/Plugin/Commerce/CheckoutPane/BillingInformationMessagePane.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\omnipedia_commerce\Plugin\Commerce\CheckoutPane;
+
+use Drupal\commerce_checkout\Plugin\Commerce\CheckoutPane\CheckoutPaneBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a billing information explanation pane for Omnipedia.
+ *
+ * @CommerceCheckoutPane(
+ *   id           = "omnipedia_billing_information_message",
+ *   label        = @Translation("Omnipedia: billing information explanation"),
+ *   default_step = "order_information",
+ * )
+ */
+class BillingInformationMessagePane extends CheckoutPaneBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildPaneForm(
+    array $paneForm, FormStateInterface $formState, array &$completeForm
+  ) {
+
+    $paneForm['#attributes'] = ['class' => ['block-help']];
+
+    $paneForm['omnipedia_billing_information_message'] = [
+      '#type'   => 'html_tag',
+      '#tag'    => 'p',
+      '#value'  => $this->t('We need your billing address to determine what taxes we\'re legally required to charge. It will not be used for any other purpose and will not be disclosed to any third-party other than the payment processor you choose.'),
+    ];
+
+    return $paneForm;
+
+  }
+
+}
